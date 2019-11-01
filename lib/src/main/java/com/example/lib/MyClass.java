34a8f3718c33c2089f6fc0d4d376cfe82be3504e
myid5478 <myid5478@gmail.com>
2019-11-01 15:21:03 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,16 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String[] args) {
+
+        for(int i = 1 ; i <=9 ;i++){
+            for(int j = 1;j<=9 ;j++){
+                System.out.print(i+"X"+j +"="+ i*j+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
